5ad2d94271877202e07a8f72701c41837da95a27
Kenta <85465075+hira-kenta@users.noreply.github.com>
2025-03-11 22:35:26 +0900

    [add] cloudflare デプロイ用の設定を追加

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,11 +12,20 @@ const config = {
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter({
-      strict: false,
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"],
+      },
+      platformProxy: {
+        enabled: undefined,
+        experimentalJsonConfig: false,
+        persist: false,
+      },
     }),
     alias: {
       "@/*": "./path/to/lib/*",
     },
+    outDir: ".svelte-kit/cloudflare",
   },
 };
 
