8962c88f5a2136f62799ded333bcc189dba4f42e
Kenta <85465075+hira-kenta@users.noreply.github.com>
2025-03-12 22:39:25 +0900

    [fix] PUBLIC_BASE_URLを削除

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { PUBLIC_BASE_URL } from "$env/static/public";
   import About from "$lib/components/about/about/About.svelte";
   import Skill from "$lib/components/about/skill/Skill.svelte";
   import Breadcrumbs from "$lib/components/Breadcrumbs.svelte";
@@ -18,7 +17,6 @@
     name="description"
     content="関西で社内SEをしているhiraaakenです。学んだことや気になったことを書きます。"
   />
-  <link rel="canonical" href={`${PUBLIC_BASE_URL}/about`} />
 </svelte:head>
 
 <Breadcrumbs {breadcrumbs} />
