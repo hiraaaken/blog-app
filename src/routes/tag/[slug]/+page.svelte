f12506e5b24500a5c1af1cbde4d4856eeab9e785
Kenta <85465075+hira-kenta@users.noreply.github.com>
2025-03-14 02:21:13 +0900

    [update] Tag 記事一覧ページのheaderを修正

--- src/routes/tag/[slug]/+page.svelte
+++ src/routes/tag/[slug]/+page.svelte
@@ -3,23 +3,23 @@
   import Breadcrumbs from "$lib/components/Breadcrumbs.svelte";
   import Heading from "$lib/components/Heading.svelte";
   import TagTip from "$lib/components/TagTip.svelte";
+  import { PUBLIC_BASE_URL } from "$env/static/public";
   import type { PageProps } from "./$types";
+  import type { Blog } from "$lib/microcms.js";
 
   let { data }: PageProps = $props();
-
+  let { name, blogs }: { name: string; blogs: Blog[] } = $derived(data);
   let breadcrumbs = $derived([
     { label: "Home", href: "/" },
     { label: "Tag", href: "/tag" },
-    { label: data.name, href: `/tag/${data.name}` },
+    { label: name, href: `/tag/${name}` },
   ]);
 </script>
 
 <svelte:head>
-  <title>Tag</title>
-  <meta
-    name="description"
-    content="hiraaakenの技術ブログです。学んだことや気になったことを書きます。"
-  />
+  <title>#{data.name} | hiraaaken's blog</title>
+  <meta name="description" content={`タグ：${name}の記事一覧です。`} />
+  <link rel="canonical" href={`${PUBLIC_BASE_URL}/tag/${name}`} />
 </svelte:head>
 
 <Breadcrumbs {breadcrumbs} />
@@ -27,14 +27,14 @@
 <section>
   <Heading>
     <div class="flex items-center gap-2 pl-2">
-      <TagTip name={data.name} size="md" link={false} />
+      <TagTip {name} size="md" link={false} />
       <span> の記事一覧 </span>
     </div>
   </Heading>
 </section>
 <section>
   <div class="grid grid-cols-12 gap-5">
-    <BlogList blogs={data.blogs.contents} />
+    <BlogList blogs={blogs.contents} />
   </div>
 </section>
 
