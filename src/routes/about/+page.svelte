07161a5c85190896296a213d93d9c1e2f7e27289
Kenta <85465075+hira-kenta@users.noreply.github.com>
2025-03-14 01:13:37 +0900

    [update] Aboutページのheaderを追加

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,8 +2,11 @@
   import About from "$lib/components/about/about/About.svelte";
   import Skill from "$lib/components/about/skill/Skill.svelte";
   import Breadcrumbs from "$lib/components/Breadcrumbs.svelte";
+  import { PUBLIC_BASE_URL } from "$env/static/public";
+  import type { PageProps } from "./$types";
 
-  let { data } = $props();
+  let { data }: PageProps = $props();
+  const { skills } = $derived(data);
 
   let breadcrumbs = [
     { label: "Home", href: "/" },
@@ -12,13 +15,14 @@
 </script>
 
 <svelte:head>
-  <title>About</title>
+  <title>About | hiraaaken's blog</title>
   <meta
     name="description"
-    content="関西で社内SEをしているhiraaakenです。学んだことや気になったことを書きます。"
+    content="関西某所で社内SEとして働いています。業務アプリの開発を行っており、フロントからバックエンドまで色々担当しています。主にフロントエンド周辺の技術に興味を持っています。"
   />
+  <link rel="canonical" href={`${PUBLIC_BASE_URL}/about`} />
 </svelte:head>
 
 <Breadcrumbs {breadcrumbs} />
 <About />
-<Skill skills={data.skills} />
+<Skill {skills} />
