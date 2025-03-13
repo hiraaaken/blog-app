c3a9a37c91947e039a17ecad601484f616aa9ad6
Kenta <85465075+hira-kenta@users.noreply.github.com>
2025-03-14 02:15:17 +0900

    [update] Tagページのheaderを修正

--- src/routes/tag/+page.svelte
+++ src/routes/tag/+page.svelte
@@ -3,10 +3,10 @@
   import Heading from "$lib/components/Heading.svelte";
   import TagTip from "$lib/components/TagTip.svelte";
   import type { PageProps } from "./$types";
+  import { PUBLIC_BASE_URL } from "$env/static/public";
 
   let { data }: PageProps = $props();
-
-  let tags = $state(data.tags.contents);
+  let { tags } = $derived(data);
 
   let breadcrumbs = [
     { label: "Home", href: "/" },
@@ -15,11 +15,12 @@
 </script>
 
 <svelte:head>
-  <title>Tag</title>
+  <title>Tag | hiraaaken's blog</title>
   <meta
     name="description"
-    content="hiraaakenの技術ブログです。学んだことや気になったことを書きます。"
+    content="hiraaakenの技術ブログです。Web開発について学んだことや気づいたことを記事にしています。"
   />
+  <link rel="canonical" href={`${PUBLIC_BASE_URL}/tag`} />
 </svelte:head>
 
 <Breadcrumbs {breadcrumbs} />
@@ -27,7 +28,7 @@
 <section class="grid gap-3">
   <Heading>タグ一覧</Heading>
   <div class="flex flex-wrap gap-4">
-    {#each tags as tag}
+    {#each tags.contents as tag}
       <TagTip name={tag.name} size="md" />
     {/each}
   </div>
