e22e90289407bda8a1c0402d03b3af899a715609
Kenta <85465075+hira-kenta@users.noreply.github.com>
2025-03-14 02:33:05 +0900

    [update] Worksページにheaderを追加

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -8,6 +8,12 @@
   ];
 </script>
 
+<svelte:head>
+  <title>Works | hiraaaken's blog</title>
+  <meta name="description" content="制作物や手がけたサービスの一覧です。" />
+  <link rel="canonical" href={`${PUBLIC_BASE_URL}/works`} />
+</svelte:head>
+
 <Breadcrumbs {breadcrumbs} />
 <Heading>Works</Heading>
 <p>随時更新予定... 🚧</p>
